3aa42d921245ea5058af2c4590ecc00722aeabe5
GrannyApples <anton.engstrom1994@gmail.com>
2026-09-01 13:22:11 +0200

    feat: kaggle EDA data

---
+++ eda/eda.py
@@ -0,0 +1,15 @@
+# Install dependencies as needed:
+# pip install kagglehub[pandas-datasets]
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+
+# Set the path to the file you'd like to load
+file_path = "solar.csv"
+
+# Load the latest version
+df = kagglehub.dataset_load(
+    KaggleDatasetAdapter.PANDAS,
+    "nasa/solar-eclipses",
+    file_path,
+)
+print("First 5 records:", df.head())
